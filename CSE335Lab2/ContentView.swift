470b5266383246576524ed063097c484409d9a6c
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-01-31 20:13:27 -0700

    Initial Commit

---
+++ CSE335Lab2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CSE335Lab2
+//
+//  Created by Arjun Dadhwal on 1/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
